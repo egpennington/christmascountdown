76b950d7ceadded10d0be103382c6da10f8f2980
egpennington (애멧) <53251137+egpennington@users.noreply.github.com>
2023-12-02 19:20:53 -0600

    Update main.js
    changed code for day, hours, minutes and seconds with interval to continously countdown

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 const countdownDisplay = document.getElementById("countdown-display")
+const countdownHours = document.getElementById("countdown-hours")
+const countdownMinutes = document.getElementById("countdown-minutes")
+const counddownSeconds = document.getElementById("countdown-seconds")
 
-function renderCountdown(){
-    const christmas = 25    
-    let now = new Date()    
-    theDay = now.getDate() 
-    console.log(theDay)
-    let daysToGo = christmas - theDay    
-    countdownDisplay.innerText = daysToGo    
+function updateCountdown() {
+    const targetDate = new Date("December 25, 2023").getTime()
+    const currentDate = new Date().getTime()
+
+    const timeDifference = targetDate - currentDate
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+    countdownDisplay.textContent = days;
+
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    countdownHours.innerHTML = `${hours} hours`
+    
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+    countdownMinutes.innerHTML = `${minutes} minutes`
+    
+    
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+    counddownSeconds.innerHTML = `${seconds} seconds`
+
+    console.log(days, hours, minutes, seconds)
 }
- 
-renderCountdown()
 
-// Stretch goals:
-// - Display hours, minutes, seconds.
-// - Add a countdown for another festival, your birthday, or Christmas 2022.
+updateCountdown();
+
+// interval to call updateCountdown every second (1000 milliseconds)
+const intervalId = setInterval(updateCountdown, 1000)
